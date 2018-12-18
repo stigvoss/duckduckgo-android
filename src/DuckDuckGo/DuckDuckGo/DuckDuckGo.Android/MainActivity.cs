c09c386a136ac4ce5604595159fb78dfd9b527d7
Stig Voss <stig@stigvoss.dk>
2018-12-18 14:52:33 +0100

    Adds splash screen

--- src/DuckDuckGo/DuckDuckGo/DuckDuckGo.Android/MainActivity.cs
+++ src/DuckDuckGo/DuckDuckGo/DuckDuckGo.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DuckDuckGo.Droid
 {
-    [Activity(Label = "DuckDuckGo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DuckDuckGo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ src/DuckDuckGo/DuckDuckGo/DuckDuckGo.Android/SplashActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace DuckDuckGo.Droid
+{
+    [Activity(Theme = "@style/SplashScreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
