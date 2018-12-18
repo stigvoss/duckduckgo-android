d5c2cd90d09d3fba52d528549cd75c468d78ee00
Stig Voss <stig@stigvoss.dk>
2018-12-18 09:38:43 +0100

    Initial code commit

---
+++ src/DuckDuckGo/DuckDuckGo/DuckDuckGo/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DuckDuckGo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (Browser.CanGoBack)
+            {
+                Browser.GoBack();
+
+                return true;
+            }
+            else
+            {
+                return base.OnBackButtonPressed();
+            }
+        }
+
+        private void OpenPage(object sender, WebNavigatingEventArgs e)
+        {
+            var url = new Uri(e.Url);
+
+            if (!url.Host.In("start.duckduckgo.com", "duckduckgo.com") && url.Scheme.In("http", "https"))
+            {
+                Device.OpenUri(url);
+                e.Cancel = true;
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static bool In<T>(this T target, params T[] list)
+        {
+            return list.Contains(target);
+        }
+    }
+}
